d58c48e2aa43d40fd1fdc0388c6067b39e98c3c5
gaetan <gs99@hotmail.fr>
2022-05-11 14:15:58 +0200

    detail pokemon

--- src/components/card/CardPokemon.js
+++ src/components/card/CardPokemon.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import './card.css'
 import { Row,Col,Card } from "react-bootstrap"
+import { useEffect,useState } from 'react'
+import { getPokemon } from "../../service/pokemonService"
 
 export default function CardPokemon(props) {
+
+    const [url,setUrl] = useState("")
+
+    useEffect(() =>{
+        getPokemon(props.id).then(res =>{
+          setUrl(res.data.sprites.front_default)
+          console.log(res.data.sprites.front_default)
+        })
+    }, [])
+
   return (
     <Col key={props.id}>
-        <Card style={{ width: '200px', height: '200px' }}>
+        <Card style={{ width: 'auto', height: 'auto' }}>
             <Card.Body>
-                <Card.Text>{props.id}</Card.Text>
-                <Card.Text>{props.name}</Card.Text>
+                <Card.Img variant="top" src={url} style={{ width: 'auto', height: 'auto' }} />
+                <Card.Text><strong>#{props.id}</strong></Card.Text>
+                <Card.Text><strong>{(props.name).toUpperCase()}</strong></Card.Text>
             </Card.Body>
         </Card>
     </Col>
